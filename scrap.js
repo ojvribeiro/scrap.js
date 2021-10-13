a8148d1eeb9300d4fdb9589f1facea01406f0e81
Victor Ribeiro <devjvribeiro@gmail.com>
2021-10-13 15:18:05 -0300

    Created getHTML method

--- scrap.js
+++ scrap.js
@@ -32,4 +32,36 @@ let scrap = {
       console.error('Traz a cloropina pro pai. :(', err)
     })
   }
+
+
+
+  getHTML: (props) => {
+    console.log('Buscando elemento...')
+    
+    fetch(proxy + props.url, fetchOptions)
+    .then((res) => {
+      // Sucesso!
+      return res.text()
+    })
+    .then((html) => {
+      // Converte a string HTML em um objeto document
+      let parser = new DOMParser()
+      let doc = parser.parseFromString(html, 'text/html')
+      
+      let htmlString = doc.querySelectorAll(props.element)[0].innerHTML
+      
+      if (props.callback !== undefined) {
+        console.log('Aí sim, meu patrão! :D')
+        
+        props.callback(htmlString)
+      }
+      else {
+        console.error('Você precisa definir uma função callback. Aí você me quebra.');
+      }
+    })
+    .catch((err) => {
+      // Erro
+      console.error('Traz a cloropina pro pai. :(', err)
+    })
+  },
 }
